929362640020ac042728ac6a5d3da7bc3c1757f8
qiurunze <qiurunze@youxin.com>
2019-01-15 19:41:47 +0800

    提交 三七

---
+++ p2p/core/src/main/java/com/eloan/base/domain/BaseDomain.java
@@ -0,0 +1,17 @@
+package com.eloan.base.domain;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author qiurunze
+ */
+@Getter
+@Setter
+public class BaseDomain implements Serializable {
+
+	protected Long id;
+
+}
---
+++ p2p/uiweb/src/main/java/com/eloan/uiweb/util/UploadUtil.java
@@ -0,0 +1,35 @@
+package com.eloan.uiweb.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 上传工具
+ * @author Administrator
+ *
+ */
+public class UploadUtil {
+
+	public static final String uploadImagePath = "e:/uploadImages";
+
+	public static String upload(MultipartFile file, String basePath) {
+//		String orgFileName = file.getOriginalFilename();
+//		String fileName = UUID.randomUUID().toString() + "."
+//				+ FilenameUtils.getExtension(orgFileName);
+//		try {
+//			File sourceFile = new File(basePath, fileName);
+//			FileUtils.writeByteArrayToFile(sourceFile, file.getBytes());
+//
+//			File targetFile = new File(uploadImagePath,
+//					FilenameUtils.getName(sourceFile.getAbsolutePath()));
+//			FileUtils.copyFile(sourceFile, targetFile);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return fileName;
+		return "";
+	}
+}
